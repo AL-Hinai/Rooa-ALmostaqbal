2a90271377f5ecb91a6c22823c2bf69c858c81ae
AL-Hinai <2233129a@student.gla.ac.uk>
2017-06-20 21:51:01 +0000

    improve url linking

--- RooaALmostaqbal/urls.py
+++ RooaALmostaqbal/urls.py
@@ -3,4 +3,7 @@ from RooaALmostaqbal import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^Booking/$',views.Booking,name='Booking'),
+    url(r'^About/$',views.About,name='About'),
+    url(r'^contact/$',views.contact,name='contact'),
 ]
--- RooaALmostaqbal/views.py
+++ RooaALmostaqbal/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index (request):
 	return render(request, 'index.html', {})
+
+def Booking (request):
+	return render(request, 'Booking.html', {})
+
+def About (request):
+	return render(request, 'About.html', {})
+
+def contact (request):
+	return render(request, 'contact.html', {})
